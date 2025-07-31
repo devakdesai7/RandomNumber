22cb1a05956276710caf4343a5b2965ab89a8901
devakdesai7 <216946260+devakdesai7@users.noreply.github.com>
2025-07-31 09:27:54 +0530

    Refactored the code for taking inputs into a function and wrote the conditional statements to compare the values given by the user with the random number and congratulate the user on guessing correctly

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,30 @@
 import random
 
+def take_input():
+    guess = input()
+    guess = int(guess)
+    return guess
+
 number = random.randint(1, 30)
 count = 0
 
-print("I am thinking of a number between 1 and 20.")
+print("I am thinking of a number between 1 and 2z0.")
 print("Take a guess.")
-guess = input()
+
+guess = take_input()
+
+while True:
+    if (guess == number):
+        count += 1
+        print(f"Good job, you guessed the number correctly in {count} guesses")
+        break
+    elif (guess < number):
+        count += 1
+        print("Your guess is too low")
+        print("Take a guess")
+        guess = take_input()
+    elif(guess > number):
+        count += 1
+        print("Your guess is too high")
+        print("Take a guess")
+        guess = take_input()
